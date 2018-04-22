c4d155ed9f606a11a3a0f74506dcd35ce2847903
modenamo <modenamo@gmail.com>
2018-04-23 01:02:35 +0300

    Add attack animations

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public const string _ATTACK_TRIGGER_1 = "Attack_1";
+    public const string _ATTACK_TRIGGER_2 = "Attack_2";
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animator.SetTrigger(Random.value > 0.5 ? _ATTACK_TRIGGER_1 : _ATTACK_TRIGGER_2);
+        }
+    }
+}
